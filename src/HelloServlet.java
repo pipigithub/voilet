96ba1895ae34a03209e5860583e4f1f9fcec0950
wendy <13816990094@163.com>
2017-04-17 13:09:04 +0800

    web代码提交

---
+++ src/HelloServlet.java
@@ -0,0 +1,43 @@
+
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class HelloServlet extends HttpServlet {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	// 重写doGet方法
+	public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String username = request.getParameter("username");
+		String password = request.getParameter("password");
+
+		// 服务器端打印信息
+		System.out.println("username=" + username);
+		System.out.println("password=" + password);
+		// 设置编码格式
+		response.setContentType("text/html;charset=GB18030");
+
+		// 返回html页面
+		response.getWriter().println("<html>");
+		response.getWriter().println("<head>");
+		response.getWriter().println("<title>登录信息</title>");
+		response.getWriter().println("</head>");
+		response.getWriter().println("<body>");
+		response.getWriter().println("欢迎【" + username + "】用户登录成功！！！");
+		response.getWriter().println("</body>");
+		response.getWriter().println("</html>");
+	}
+
+	// 重写doPost方法
+	public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+}
